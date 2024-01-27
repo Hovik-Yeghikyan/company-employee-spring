7d6042a78d4c9dd4506ce266b9a15b5fef1c656b
Hovik Yeghikyan <hovikyeghikyan@gmail.com>
2024-01-27 19:59:33 +0300

    Initial commit

---
+++ src/main/java/com/example/companyemployee/entity/Company.java
@@ -0,0 +1,16 @@
+package com.example.companyemployee.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "company")
+@Data
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+}
